969fba15e7556dc0b8464720803d1260d8ed7009
Stephen Fox <stephenfox995@hotmail.com>
2018-12-10 23:10:43 +0000

    Updated javadoc

--- src/main/java/com/stephenfox/pumpkin/http/PumpkinHttpSocketListener.java
+++ src/main/java/com/stephenfox/pumpkin/http/PumpkinHttpSocketListener.java
@@ -8,6 +8,13 @@ import java.util.Queue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class loops forever waiting for incoming tcp connections via socket. This class acts as a
+ * producer of incoming tcp connections by placing them into a shared request queue. The socket
+ * object associated with the connection is placed into the queue for further processing.
+ *
+ * @author Stephen Fox.
+ */
 class PumpkinHttpSocketListener implements HttpSocketListener {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(PumpkinHttpSocketListener.class);
@@ -15,6 +22,14 @@ class PumpkinHttpSocketListener implements HttpSocketListener {
   private final String host;
   private final Queue<Socket> requestQueue;
 
+  /**
+   * Creates a new listener.
+   *
+   * @param host The host to run on.
+   * @param port The port to listen on.
+   * @param requestQueue A queue where the incoming connections will be placed, this queue should be
+   *     thread safe.
+   */
   PumpkinHttpSocketListener(String host, int port, Queue<Socket> requestQueue) {
     this.host = host;
     this.port = port;
